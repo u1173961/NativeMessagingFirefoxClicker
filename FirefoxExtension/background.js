1177c6d1a9a336905acc30178fbf4fa7da1133dc
Rex <laurencehunter@hotmail.com>
2026-06-05 12:05:56 +0100

    visibility check and indentation

--- FirefoxExtension/background.js
+++ FirefoxExtension/background.js
@@ -4,60 +4,59 @@ let port = null;
 let attempted = false;
 
 browser.runtime.onMessage.addListener((msg) => {
-	console.log("background received:", msg);
-	if (!(msg.type === "nativeClick" && msg.app_key)) {
-    console.log("example@rex.com: unexpected onMessage()");
-		return Promise.resolve({ ok: false, error: "Unexpected message" });
-	}
+    console.log("background received:", msg);
+    if (!(msg.type === "nativeClick" && msg.app_key)) {
+        console.log("example@rex.com: unexpected onMessage()");
+        return Promise.resolve({ ok: false, error: "Unexpected message" });
+    }
+    try {
+        if (!attempted) {
+            connect();
+            if (!port) {
+                console.log("example@rex.com native host is not connected");
+                return;
+            }
+            attempted = true;
 
-	try {
-		if (!attempted) {
-      connect();
-      if (!port) {
-        console.log("example@rex.com native host is not connected");
-        return;
-      }
-			attempted = true;
-
-      console.log("Sending to example@rex.com native host");
-			port.postMessage({
-				type: "click",
-				app_key: 1,
-				x: msg.x,
-				y: msg.y
-			});
-			setTimeout(function () {
-				attempted = false;
-			}, 1000);
-		}
-		return Promise.resolve({ ok: true });
-	} catch (e) {
-		console.error("native click failed:", e);
-			setTimeout(function () {
-				attempted = false;
-			}, 3500);
-		return Promise.resolve({ ok: false, error: String(e) });
-	}
+            console.log("Sending to example@rex.com native host");
+            port.postMessage({
+                type: "click",
+                app_key: 1,
+                x: msg.x,
+                y: msg.y
+            });
+            setTimeout(function () {
+                attempted = false;
+            }, 1000);
+        }
+        return Promise.resolve({ ok: true });
+    } catch (e) {
+        console.error("native click failed:", e);
+        setTimeout(function () {
+            attempted = false;
+        }, 3500);
+        return Promise.resolve({ ok: false, error: String(e) });
+    }
 });
 
 function connect() {
-  if (!port) {
-    console.log("Connecting to NativeHostClicker");
-    port = browser.runtime.connectNative("NativeHostClicker");
+    if (!port) {
+        console.log("Connecting to NativeHostClicker");
+        port = browser.runtime.connectNative("NativeHostClicker");
 
-    port.onMessage.addListener((response) => {
-      console.log("example@rex.com background.js received:", response);
-    });
+        port.onMessage.addListener((response) => {
+            console.log("example@rex.com background.js received:", response);
+        });
 
-    port.onDisconnect.addListener((disconnectedPort) => {
-      if (disconnectedPort.error) {
-        console.log(`example@rex.com disconnected due to an error: ${disconnectedPort.error.message}`);
-      } else {
-        // If this happens right after connectNative(), Firefox probably could not
-        // find, launch, or authorize the native messaging host.
-        console.log("example@rex.com background.js disconnected", disconnectedPort);
-      }
-        port = null;
-      });
-  }
+        port.onDisconnect.addListener((disconnectedPort) => {
+            if (disconnectedPort.error) {
+                console.log(`example@rex.com disconnected due to an error: ${disconnectedPort.error.message}`);
+            } else {
+                // If this happens right after connectNative(), Firefox probably could not
+                // find, launch, or authorize the native messaging host.
+                console.log("example@rex.com background.js disconnected", disconnectedPort);
+            }
+            port = null;
+        });
+    }
 }
--- FirefoxExtension/skipper.js
+++ FirefoxExtension/skipper.js
@@ -1,60 +1,60 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-let attempts = 0;
+let skipAttempts = 0;
 let changing = false;
-let observer = new MutationObserver(function(mutations, observer) {
-	let ads = document.querySelectorAll('.ad-showing');
-	if (ads.length > 0) {
-		const mute = document.querySelector('.ytp-volume-icon');
-		if (mute.getAttribute('data-title-no-tooltip') === 'Mute') {
-			changing = true;
-			mute.click();
-		}
-		const skip = document.querySelector('.ytp-skip-ad-button');
-		if (skip) {
-			if (attempts < 1) {
-				attempts++;
-				let xy = getScreenCenter(skip);
-				console.log("xy:", xy);
-				browser.runtime.sendMessage({
-				  type: "nativeClick",
-				  app_key: 1,
-				  x: xy.x,
-				  y: xy.y
-				}).then(response => {
-				  console.log("Response:", response);
-				}).catch(error => {
-				  console.error("sendMessage error:", error);
-				});
-				setTimeout(function() {
-					attempts = 0;
-				}, 600);
-			}
-		}
-	} else if (changing) {
-		const mute = document.querySelector('.ytp-volume-icon');
-		if (mute.getAttribute('data-title-no-tooltip') === 'Unmute') {
-			mute.click();
-		}
-		const play = document.querySelector('.ytp-play-button');
-		if (play.getAttribute('data-title-no-tooltip') === 'Play') {
-			play.click();
-		}
-		changing = false;
-	}
+let observer = new MutationObserver(function (mutations, observer) {
+    let ads = document.querySelectorAll('.ad-showing');
+    if (ads.length > 0) {
+        const mute = document.querySelector('.ytp-volume-icon');
+        if (mute.getAttribute('data-title-no-tooltip') === 'Mute') {
+            changing = true;
+            mute.click();
+        }
+        if (skipAttempts < 1) {
+            const skip = document.querySelector('.ytp-skip-ad-button');
+            if (skip && skip.checkVisibility()) {
+                skipAttempts++;
+                let xy = getScreenCenter(skip);
+                console.log("xy:", xy);
+                browser.runtime.sendMessage({
+                    type: "nativeClick",
+                    app_key: 1,
+                    x: xy.x,
+                    y: xy.y
+                }).then(response => {
+                    console.log("Response:", response);
+                }).catch(error => {
+                    console.error("sendMessage error:", error);
+                });
+                setTimeout(function () {
+                    skipAttempts = 0;
+                }, 600);
+            }
+        }
+    } else if (changing) {
+        const mute = document.querySelector('.ytp-volume-icon');
+        if (mute.getAttribute('data-title-no-tooltip') === 'Unmute') {
+            mute.click();
+        }
+        const play = document.querySelector('.ytp-play-button');
+        if (play.getAttribute('data-title-no-tooltip') === 'Play') {
+            play.click();
+        }
+        changing = false;
+    }
 });
 observer.observe(document, {
-  subtree: true,
-  attributes: true,
-  childList: true
+    subtree: true,
+    attributes: true,
+    childList: true
 });
 
 function getScreenCenter(el) {
-  const rect = el.getBoundingClientRect();
-  const dpr = window.devicePixelRatio || 1;
+    const rect = el.getBoundingClientRect();
+    const dpr = window.devicePixelRatio || 1;
 
-  return {
-    x: Math.round((window.mozInnerScreenX + rect.left + rect.width / 2) * dpr),
-    y: Math.round((window.mozInnerScreenY + rect.top + rect.height / 2) * dpr)
-  };
+    return {
+        x: Math.round((window.mozInnerScreenX + rect.left + rect.width / 2) * dpr),
+        y: Math.round((window.mozInnerScreenY + rect.top + rect.height / 2) * dpr)
+    };
 }
